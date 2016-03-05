4e0cfc15348ed044901acf09f212a441145030aa
Oskar Gewalli <oskar@gewalli.se>
2016-03-05 07:42:20 +0100

    public isDirty

--- src/scripts/viewModel/CustomerViewModel.ts
+++ src/scripts/viewModel/CustomerViewModel.ts
@@ -2,15 +2,22 @@
 /// <reference path="../../../_declare/bluebird.d.ts" />
 /// <amd-dependency path="knockout" />
 var ko = require("knockout");
+export interface ICustomerViewModel { 
+    isDirty:boolean;
+    firstName:string;
+    lastName:string;
+}
 
-export class CustomerViewModel {
+export class CustomerViewModel implements ICustomerViewModel {
     private _isDirty = ko.observable(false);
     private _customer;
+    public isDirty;
     constructor(customer) {
         this._customer = customer;
+        Object.defineProperties(this,{
+            isDirty:{get:this._isDirty, set:this._isDirty}
+        });
     }
-    get isDirty() { return this._isDirty(); }
-    set isDirty(value) { this._isDirty(value); }
     get firstName() { return this._customer.firstName; }
     set firstName(value) { this._customer.firstName = value; this.isDirty = true; }
     get lastName() { return this._customer.lastName; }
